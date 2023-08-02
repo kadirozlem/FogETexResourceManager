d379d4a897c55c9cbe4ea99764d184f21be3117e
kadirozlem <kadirozlem@hotmail.com>
2023-08-02 14:30:42 +0300

    External User get Broker and Worker Resource

--- SocketServer.js
+++ SocketServer.js
@@ -1,15 +1,28 @@
 const {Server, Socket} = require("socket.io");
 const io = new Server();
 let Config = require("./Config");
+let SocketClient = require("./SocketClient")
 io.resource_members = [];
+
+io.socket_clients ={};
 
 io.on("connection", (socket) => {
     console.log("User connected! Socket ID: "+socket.id);
     io.resource_members.push(socket)
-
+    if(socket.handshake.query.URL || false){
+        socket.worker_url = socket.handshake.query.URL;
+        if (io.socket_clients[socket.worker_url]){
+            io.socket_clients.AddMember(socket)
+        }else{
+            io.socket_clients[socket.worker_url] = new SocketClient(socket.worker_url, socket, io);
+        }
+    }
     socket.on("disconnect", (reason) => {
 
         console.log("User disconnected " + reason)
+        if(socket.worker_url){
+            io.socket_clients[socket.worker_url].RemoveMember(socket)
+        }
 
         var resource_index = io.resource_members.indexOf(socket);
         if (resource_index !== -1) {
